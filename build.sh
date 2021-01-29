16f8dc2c5016aad85da6aade1774d8efb9c4f8fb
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2021-01-29 10:57:33 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get update
-sudo apt-get install -y git tar fakeroot libssl-dev libcap2-bin autoconf automake libtool build-essential
-git clone --recursive https://github.com/RIPE-NCC/ripe-atlas-software-probe.git -b probev5
-./ripe-atlas-software-probe/build-config/debian/bin/make-deb
